aa47688684099a6432d42f5430192e26df58b92a
T4RYFA <miroslaw.kusiak@smcebi.edu.pl>
2016-11-07 10:30:22 +0100

    Changed precision, ended line

--- area.cpp
+++ area.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main( ) 
 { 
-    float PI = 3.14;                // variables can be initialized during declaration 
+    float PI = 3.1415926;                // variables can be initialized during declaration 
     integer rad; 
-    cout<< "Enter the radius"; 
+    cout<< "Enter the radius :"<<endl; 
     cin>>rad; 
     cout<< "Area of the circle is: "<< PI * rad * rad; 
     return 0;
