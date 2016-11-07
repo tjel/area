bf5cf10738250b87fa98d1498448979781313167
Trishun <piotr_dec@msn.com>
2016-11-07 11:03:05 +0100

    error

--- area.cpp
+++ area.cpp
@@ -3,22 +3,22 @@
 
 using namespace std;
 
-<<<<<<< HEAD
-int main( ) 
-{ 
-    float PI = 3.1415;                // variables can be initialized during declaration 
-    double rad; 
-    cout<< "Enter the radius in cm: "; 
-    cin>>rad; 
+
+int main( )
+{
+    float PI = 3.1415;                // variables can be initialized during declaration
+    double rad;
+    cout<< "Enter the radius in cm: ";
+    cin>>rad;
 	if (rad < 0){
 	cout<< "Radius has tobe positive number \n";
 	}
 	else{
-    		cout<< "Area of the circle is: "<< PI * rad * rad << " cm\n"; 
+    		cout<< "Area of the circle is: "<< PI * rad * rad << " cm\n";
     		return 0;
 	}
-}  
-=======
+}
+
 int main( )
 {
     float PI = 3.14;                // variables can be initialized during declaration
