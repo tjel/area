b4905062a8e970296d64c251f2794ebca41b2eb7
Arek Otto <arekotto@Enterpryzes-MacBook-Pro.local>
2016-11-07 10:35:07 +0100

    zmiany

--- area.cpp
+++ area.cpp
@@ -5,9 +5,12 @@ using namespace std;
 int main( ) 
 { 
     float PI = 3.14;                // variables can be initialized during declaration 
-    integer rad; 
+    double rad;
     cout<< "Enter the radius"; 
-    cin>>rad; 
+    cin>>rad;
+    if (rad < 0) {
+        cout << "Radius has to be a positive real number. Please correct your input.";
+    }
     cout<< "Area of the circle is: "<< PI * rad * rad; 
     return 0;
 }  
