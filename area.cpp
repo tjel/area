e3f9aec3f58b6f3b52a8588cf7a5b0fcc703d5fa
T4RYFA <miroslaw.kusiak@smcebi.edu.pl>
2016-11-07 10:37:58 +0100

    added if statement

--- area.cpp
+++ area.cpp
@@ -5,9 +5,11 @@ using namespace std;
 int main( ) 
 { 
     float PI = 3.1415926;                // variables can be initialized during declaration 
-    integer rad; 
+    double rad; 
     cout<< "Enter the radius :"<<endl; 
     cin>>rad; 
+    if (rad < 0){
+        cout>> "Radius has to be a positive real number. Please correct your input."}
     cout<< "Area of the circle is: "<< PI * rad * rad; 
     return 0;
 }  
