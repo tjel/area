dcb74d2ec085f4b19c7acc896da1e39474c812b4
jakub-borowka <qbabor4444@gmail.com>
2016-11-07 11:23:53 +0100

    ok3

--- area.cpp
+++ area.cpp
@@ -7,7 +7,7 @@ int main( )
 { 
     float PI = 3.1415;                // variables can be initialized during declaration 
     double rad; 
-    cout<< "Enter the radius in cm: "; 
+    cout<< "Enter the radius in cm: \n>>"; 
     cin>>rad;
     if(rad < 0) {
 	cout<<"Radius has to be a positive real number. Please correct your input\n";
