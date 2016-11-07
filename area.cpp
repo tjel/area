7ab1eedcf914afa02a36a517906a255d2b93e667
jakub-borowka <qbabor4444@gmail.com>
2016-11-07 10:36:13 +0100

    addiction

--- area.cpp
+++ area.cpp
@@ -1,12 +1,13 @@
+
 # include <iostream>
 
 using namespace std;
 
 int main( ) 
 { 
-    float PI = 3.14;                // variables can be initialized during declaration 
-    integer rad; 
-    cout<< "Enter the radius"; 
+    float PI = 3.1415;                // variables can be initialized during declaration 
+    int rad; 
+    cout<< "Enter the radius in cm: "; 
     cin>>rad; 
     cout<< "Area of the circle is: "<< PI * rad * rad; 
     return 0;
