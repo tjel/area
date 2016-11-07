9f9047aa51bb098f2baecc65f5dc6b21e661a08e
jakub-borowka <qbabor4444@gmail.com>
2016-11-07 11:18:57 +0100

    ok2

--- area.cpp
+++ area.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-<<<<<<< HEAD
 int main( ) 
 { 
     float PI = 3.1415;                // variables can be initialized during declaration 
@@ -16,17 +15,5 @@ int main( )
     else {
         cout<< "Area of the circle is: "<< PI * rad * rad << "cm\n"; 
 	}
-
-=======
-int main( )
-{
-    double PI = 3.14159265359;                // variables can be initialized during declaration
-    double rad;
-    cout<< "Enter the radius"<<endl;
-    cin>>rad;
-    if (rad>0){
-        cout<< "Area of the circle is: "<< PI * rad * rad<<endl;}
-    else {
-        cout<<"Radius has to be a positive real number. Please correct your input."<<endl;}
->>>>>>> refs/remotes/origin/master
 }
+
